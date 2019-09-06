7cf8ca1401ec9faf75951f9596ddfa25ccff9a1a
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-09-06 17:17:25 -0600

    no errors

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const path = require("path");
 
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 module.exports = router;
