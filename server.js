14875b7578c5f81ca1783b478064220c4d3065f1
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-09-28 09:30:55 -0600

    Heroku build

--- server.js
+++ server.js
@@ -6,25 +6,20 @@ let PORT = process.env.PORT || 5000;
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client/build")));
-// app.use(express.static("client/build"));
-//app.use(express.static("public"));
 // parse application/json
+app.use(bodyParser.json());
 
 const routes = require("./routes");
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  console.log("prod?");
-  app.use(express.static("client/build"));
-  app.use(express.static("public"));
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  // console.log("catch all routes and make sure they work!!");
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
