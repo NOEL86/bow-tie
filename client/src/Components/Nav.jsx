858b319fd8ba638e780816bc62f0b5ae1bceb2f5
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-10-06 20:22:52 -0600

    social media tags working

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -96,26 +96,34 @@ class Nav extends Component {
               </li>
             </ul>
             <div id="sMediaIcons" className="row right">
-              <img
-                style={{ width: "30px" }}
-                src="../images/icons8-facebook-f-50.png"
-                alt="FB Logo"
-              />
-              <img
-                style={{ width: "30px" }}
-                src="../images/icons8-pinterest-50.png"
-                alt="Pintrest Logo"
-              />
-              <img
-                style={{ width: "30px" }}
-                src="../images/icons8-instagram-old-50.png"
-                alt="Pintrest Logo"
-              />
-              <img
-                style={{ width: "30px" }}
-                src="../images/icons8-snapchat-50.png"
-                alt="Snapchat Logo"
-              />
+              <a href="https://www.facebook.com/Bow-Tie-Events-122658612466871/">
+                <img
+                  style={{ width: "30px" }}
+                  src="../images/icons8-facebook-f-50.png"
+                  alt="FB Logo"
+                />
+              </a>
+              {/* <a href="">
+                <img
+                  style={{ width: "30px" }}
+                  src="../images/icons8-pinterest-50.png"
+                  alt="Pintrest Logo"
+                />
+              </a> */}
+              <a href="https://www.instagram.com/bowandtieeventswa/">
+                <img
+                  style={{ width: "30px" }}
+                  src="../images/icons8-instagram-old-50.png"
+                  alt="Pintrest Logo"
+                />
+              </a>
+              {/* <a href="">
+                <img
+                  style={{ width: "30px" }}
+                  src="../images/icons8-snapchat-50.png"
+                  alt="Snapchat Logo"
+                />
+              </a> */}
             </div>
           </div>
         </nav>
