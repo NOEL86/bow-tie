5db571273121fa4a7dcb98b17040028f27416178
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-11-16 11:27:25 -0700

    catch all changed added err

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   // app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+   
   });
-}
+});
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
