5a9e1161757735676a56700e02d2814370522bd0
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-11-16 11:43:17 -0700

    changed server to public/index

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   // app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"), function(err) {
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public_html/index.html"), function(err) {
       if (err) {
         res.status(500).send(err)
       }
