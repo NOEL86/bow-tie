736415b6edef9054f9f518992fad739b9c725dea
Ashley jimenez <ashleyjimenez@Ashleys-MacBook-Pro.local>
2019-11-21 21:00:19 -0700

    changed path join to resolve

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use(routes);
 // Serve any static files
 // app.use(express.static(path.join(__dirname, "client/build")));
 // Handle React routing, return all requests to React app
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "index.html"));
+// });
+
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"), function(err) {
+  res.sendFile(path.resolve(__dirname, "index.html"), function(err) {
     if (err) {
       res.status(500).send(err);
     }
